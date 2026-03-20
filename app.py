ea9b7ededf5f52df36c6c4c529ab9e33b7ab3551
arif <lunix.arif@gmail.com>
2026-03-20 16:31:10 +0700

    update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 # =========================
 # LOGIN SYSTEM
 # =========================
-st.caption("ROP • EOQ • Safety Stock Analysis")
+st.title("📦 Inventory Analysis (ROP, EOQ, Safety Stock)")
 
 
 def login():
@@ -38,7 +38,7 @@ if st.button("Logout"):
     st.rerun()
 
 # Upload file
-st.caption("ROP • EOQ • Safety Stock Analysis")
+st.title("📦 Inventory Analysis (ROP, EOQ, Safety Stock)")
 
 uploaded_file = st.file_uploader("Upload file Excel", type=["xlsx"])
 
