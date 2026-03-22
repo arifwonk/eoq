7d0173b7914557ce1cb2253185ba4eb8a8572ffc
arif <lunix.arif@gmail.com>
2026-03-22 11:32:28 +0700

    update

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ st.title("📦 Inventory Analysis (ROP, EOQ, Safety Stock)")
 
 
 def check_login(username, password):
-    response = supabase.table("users")\
-        .select("*")\
-        .eq("username", username)\
-        .eq("password", password)\
+    response = supabase.table("users") \
+        .select("*") \
+        .eq("username", username) \
+        .eq("password", password) \
         .execute()
 
     if len(response.data) > 0:
@@ -29,17 +29,18 @@ def check_login(username, password):
 
 
 def login():
-    st.title("🔐 Login")
+    def login():
+        st.title("🔐 Login Supabase")
 
-    username = st.text_input("Username")
-    password = st.text_input("Password")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
 
-    if st.button("Login"):
-        if check_login(username, password):
-            st.session_state["login"] = True
-            st.session_state["users"] = username
-        else:
-            st.error("Login Gagal")
+        if st.button("Login"):
+            if check_login(username, password):
+                st.session_state["login"] = True
+                st.session_state["user"] = username
+            else:
+                st.error("Login gagal")
 
 
 # cek login (PROTECT APP)
